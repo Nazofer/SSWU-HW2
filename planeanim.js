19e9b4553ab010284d69e3f2e0a8bbc0a1d8df6d
Nazar Vovk <nvovk.2004@gmail.com>
2023-01-27 00:16:49 +0200

    burger-menu, instagram icon

--- planeanim.js
+++ planeanim.js
@@ -6,14 +6,10 @@ const [plane1, plane2] = document.querySelectorAll('.plane');
 
 plane1.addEventListener('animationend', () => {
   plane1.classList.remove('animated');
-  plane2.classList.remove('animated');
   plane1.classList.add('get-back');
-  plane2.classList.add('get-back');
 });
 
 plane1.addEventListener('mouseover', () => {
   plane1.classList.remove('get-back');
-  plane2.classList.remove('get-back');
   plane1.classList.add('animated');
-  plane2.classList.add('animated');
 });
